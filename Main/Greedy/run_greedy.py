0c85afe2943d6e9e85cd16467f31ff9ca4c88c19
Ao <160457051+William-A-Wang@users.noreply.github.com>
2024-10-13 19:09:20 -0400

    Update run_greedy.py

--- Main/Greedy/run_greedy.py
+++ Main/Greedy/run_greedy.py
@@ -4,21 +4,21 @@ import os
 def run_programs():
     python_venv = "D:\\CISEProgram\\USDA\\.venv\\Scripts\\python.exe"  
 
-    subprocess.run([python_venv, "D:\\CISEProgram\\USDA\\Greedy\\Extract3.py"], check=True)
+    subprocess.run([python_venv, "D:\\CISEProgram\\USDA\\Greedy\\Extract_V3.py"], check=True)
     print("Program 1 finished, E-output.txt generated.")
 
     if not os.path.exists("D:\\CISEProgram\\USDA\\Greedy\\E_output.txt"):
         print("Error: E_output.txt not found!")
         return
 
-    subprocess.run([python_venv, "D:\\CISEProgram\\USDA\\Greedy\\V5.py"], check=True)
+    subprocess.run([python_venv, "D:\\CISEProgram\\USDA\\Greedy\\Badness_Cal_V6.py"], check=True)
     print("Program 2 finished, badness_matrix.csv generated.")
 
     if not os.path.exists("D:\\CISEProgram\\USDA\\Greedy\\badness_matrix.csv"):
         print("Error: badness_matrix.csv not found!")
         return
 
-    subprocess.run([python_venv, "D:\\CISEProgram\\USDA\\Greedy\\GV5.py"], check=True)
+    subprocess.run([python_venv, "D:\\CISEProgram\\USDA\\Greedy\\Greedy_V5.py"], check=True)
     print("Program 3 finished, final processing done.")
 
 if __name__ == "__main__":
