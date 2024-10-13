0d3c5b2fc1d68b88d97a3ca3971d090911e4e76d
Ao <160457051+William-A-Wang@users.noreply.github.com>
2024-10-13 18:27:58 -0400

    Update and rename run.py to run_greedy.py

--- Main/Greedy/run_greedy.py
+++ Main/Greedy/run_greedy.py
@@ -2,13 +2,13 @@ import subprocess
 
 def run_programs():
   
-    subprocess.run(["python3", "Extract3.py"])
+    subprocess.run(["python3", "Extract_V3.py"])
     print("Program 1 finished, E-output.txt generated.")
 
-    subprocess.run(["python3", "V5.py"])
+    subprocess.run(["python3", "Badness_Cal_V6.py"])
     print("Program 2 finished, badness_matrix.csv generated.")
 
-    subprocess.run(["python3", "GV5.py"])
+    subprocess.run(["python3", "Greedy_V5.py"])
     print("Program 3 finished, final processing done.")
 
     try:
