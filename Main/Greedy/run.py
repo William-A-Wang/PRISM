628d56c060009315a42e9555ee33fa68745b7348
Ao <160457051+William-A-Wang@users.noreply.github.com>
2024-10-13 18:26:15 -0400

    Add files via upload

---
+++ Main/Greedy/run.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def run_programs():
+  
+    subprocess.run(["python3", "Extract3.py"])
+    print("Program 1 finished, E-output.txt generated.")
+
+    subprocess.run(["python3", "V5.py"])
+    print("Program 2 finished, badness_matrix.csv generated.")
+
+    subprocess.run(["python3", "GV5.py"])
+    print("Program 3 finished, final processing done.")
+
+    try:
+        with open("optimized_primers.txt", "r") as file:
+            content = file.read()
+            print("Content of optimized_primers.txt:")
+            print(content)
+    except FileNotFoundError:
+        print("Error: optimized_primers.txt not found.")
+
+if __name__ == "__main__":
+    run_programs()
