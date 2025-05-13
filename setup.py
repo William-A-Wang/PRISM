d616e1f680ba67b3923b85b2251be89512810bf3
Ao Wang <william.ao.wang@gmail.com>
2025-05-12 22:43:27 -0400

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="prism",
+    name="prism-bio",
     version="1.0.1",
     description="PCR primer design & optimization pipeline",
     author="Ao Wang",
